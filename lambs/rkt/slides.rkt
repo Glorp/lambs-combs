ced52fa6b8f93af3049e00064287317d8edef743
Jonas Winje <jonas.winje@gmail.com>
2019-05-04 13:15:45 +0200

    stuff

--- lambs/rkt/slides.rkt
+++ lambs/rkt/slides.rkt
@@ -17,8 +17,8 @@
          "like, cursor somewhere on next line and do C-e:"
          "I ≜ λx.x"
          ""
-         "C-S-e: evaluate until normal form, or like at least 1000 steps"
-         "C-r: replace names of things you have defined with their definitions"
+         "C-S-e: evaluate until normal form, or at least 1000 steps"
+         "C-r: replace names of things defined with their definitions"
          ""
          "like, cursor on next line, C-r, then C-e or C-S-e:"
          "I I I I foo"
@@ -66,28 +66,32 @@
          ""
          "(λa.λb.λc.a c c) foo bar quux")
 
-        (churchnums
+        (lambda-churchnums
          "nums like:"
-         "λf.λx.x"
-         "λf.λx.f x"
-         "λf.λx.f (f x)"
-         "λf.λx.f (f (f (f (f x))))"
+         "l0 ≜ λf.λx.x"
+         "l1 ≜ λf.λx.f x"
+         "l2 ≜ λf.λx.f (f x)"
+         "l3 ≜ λf.λx.f (f (f x))"
+         "l5 ≜ λf.λx.f (f (f (f (f x))))"
          ""
          "and like functions like successor"
-         "λn.λf.λx.f (n f x)"
+         "lsuc ≜ λn.λf.λx.f (n f x)"
          "and plus"
-         "λa.λb.λf.λx.a f (b f x)"
+         "l+ ≜ λa.λb.λf.λx.a f (b f x)"
          "and stuff")
 
         (schon
-         "  It is in the spirit of the axiomatic method as it has now received recognition,"
-         "  chiefly through the work of Hilbert,"
-         "  that we not only strive to keep the axioms as few and their content as limited as possible"
-         "  but also attempt to make the number of fundamental undefined notions as small as we can;"
-         "  we do this by seeking out those notions from which we shall best be able"
-         "  to construct all other notions of the branch of science in question."
+         "  It is in the spirit of the axiomatic method as it has now"
+         "  received recognition, chiefly through the work of Hilbert,"
+         "  that we not only strive to keep the axioms as few and"
+         "  their content as limited as possible but also attempt to"
+         "  make the number of fundamental undefined notions as small"
+         "  as we can; we do this by seeking out those notions from"
+         "  which we shall best be able to construct all other notions"
+         "  of the branch of science in question."
          ""
-         "  Moses Schönhinkel, On the building blocks of mathematical logic")
+         "  On the building blocks of mathematical logic"
+         "  Moses Schönhinkel")
 
         (combs
          "for like our purposes:"
@@ -104,9 +108,12 @@
         (comb-rules
          "but also syntax:"
          "I x ≜ x"
+         "BLAH x y z ≜ BLAH z y x"
          ""
-         "And then e.g."
-         "I foo")
+         "and then e.g."
+         "I foo"
+         "BLAH 1 2 3"
+         "BLAH 1 2")
 
         (ski
          "S x y z ≜ (x z) (y z)"
@@ -119,9 +126,15 @@
          "(or I ≜ λx.x)")
 
         (iks
-         "I is the identity function"
+         "I x ≜ x"
+         "I is the identity function"         
+         ""
+         "K x y ≜ x"
          "we can use K to make constant functions"
-         "S is like, stuff...")
+         "e.g. (K 1) is the function that always returns 1"
+         ""
+         "S x y z ≜ (x z) (y z)"
+         "S is like, oof, stuff...")
 
         (try-to-ski
          "maybe doable:"
@@ -130,10 +143,37 @@
          "λa.λb.λc.c"
          ""
          "maybe harder:"
-         "λa.λb.a"
-         "λa.λb.λc.c"
-         "λf.λx.f x")
+         "λf.λx.f x"
+         "λa.λb.λc.b")
 
+        (tried-to-ski
+         "translate by gradually rewriting and testing with dummy args"
+         ""
+         "\"abstraction elimination\""
+         ""
+         "replace a lambda abstraction with a combinator that's been"
+         "given all but one of its arguments"
+         ""
+         "I and K not so bad"
+         "λx.x                    => I"
+         "λx.u, if no free x in u => K u")
+        
+        (s
+         "S x y z ≜ (x z) (y z)"
+         ""
+         "in translating we pass inn two arguments (all but one)"
+         "so the z will kind of replace the original lambda-parameter"
+         ""
+         "function applications consists of two things"
+         "function-part-thing and argument-part-thing"
+         ""
+         "the z will then be passed into both so _those_ need to be"
+         "made into functions accepthing the original lambda parameter"
+         ""
+         "so:"         
+         "λf.λx.f x"
+         "λa.λb.λc.b")
+        
         (ski-rules
          "λx.x                    => I"
          "λx.u, if no free x in u => K u"
@@ -157,11 +197,26 @@
          "(an expression with free variables is open."
          "an expression with no free variables is closed.)")
 
+        (eta
+         "λx.f x is the same as f as long as there is no free x in x")
+        
         (ski-eta-rules
          "λx.x                      => I"
          "λx.u, if no free x in u   => K u"
          "λx.u x, if no free x in u => u"
          "λx.u1 u2                  => S (λx.u1) (λx.u2)")
+        
+        (many-ks
+         "while translating we often get stuff like"
+         "λf.λx.K (f x)"
+         "which we then turn into"
+         "λf.S (K K) f"
+         ""
+         "and the K K bit seems maybe silly"
+         "like there was nothing really important going on in there?"
+         ""
+         "would be nice if we could just like _not_ throw additional"
+         "Ks onto uninteresting stuff")
         
         (bc
          "B x y z ≜ x (y z)"
@@ -175,6 +230,10 @@
          "          if no free x in u1 => B u1 (λx.u2)"
          "          if no free x in u2 => C (λx.u1) u2"
          "          else               => S (λx.u1) (λx.u2)")
+
+        (fewer
+         "we could do like S K K instead of I"
+         "there's some stuff about")
 
         (hask
          "foo x y = (x + y) / 2")
